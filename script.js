a2032efda3e97e8c0967bdff78bf03aade8e1ec0
SimpleaRU <info@simplea.ru>
2018-01-28 09:05:52 +0300

    inputs

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var pathFirstArrow = document.querySelector('.path-first-arrow');
 var arrows = document.querySelector('.arrows');
 var pathSecondArrow = document.querySelector('.path-second-arrow');
 var firstNumberText = document.querySelector('.exercise-first-number');
+var firstNumberInput = document.querySelector('.first-num-input');
 var secondNumberText = document.querySelector('.exercise-second-number');
 var firstNumber, secondNumber, sum;
 var n1x2, n1x3, n2x2, n2x3;
@@ -18,28 +19,34 @@ function getCoordinate(num) {
 firstNumber = getRandomInt(6, 10);
 sum = getRandomInt(11, 15);
 
-
 n1x3 = getCoordinate(firstNumber);
 n1x2 = n1x3 / 2;
 
 n2x3 = getCoordinate(sum);
-
 n2x2 = (n2x3 - n1x3) / 2 + n1x3;
 
 firstNumberText.innerHTML = firstNumber;
 secondNumberText.innerHTML = sum - firstNumber;
 
 pathFirstArrow.setAttribute('d', `M 0 150 Q ${n1x2} 0 ${n1x3} 150`);
-console.log(firstNumber)
-console.log(n1x3 );
-
-
 pathFirstArrow.classList.add('animation');
 
-document.addEventListener('click', function () {
+firstNumberInput.style.left = (n1x2 + 12 ) + 'px';
+firstNumberInput.value = '';
 
-    pathSecondArrow.setAttribute('d', `M ${n1x3} 150 Q ${n2x2} 0 ${n2x3} 150`);
-    pathSecondArrow.classList.add('animation');
+
+
+firstNumberInput.addEventListener('keyup', function (e) {
+
+    console.log(firstNumberInput.value);
+    if (firstNumberInput.value == firstNumber) {
+        pathSecondArrow.setAttribute('d', `M ${n1x3} 150 Q ${n2x2} 0 ${n2x3} 150`);
+        pathSecondArrow.classList.add('animation');
+    }
+    else {
+        firstNumberInput.style.color = 'red';
+        firstNumberText.style.background = 'yellow';
+    }
 
 })
 
